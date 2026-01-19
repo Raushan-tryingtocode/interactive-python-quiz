b7d93cd45263f7169f5065bf54299f4483d85f43
Mohammed Raushan Hussain <raushanh31@gmail.com>
2026-01-20 01:16:23 +0530

    refactor : used a list of dictionaries for better data handling

--- quiz.py
+++ quiz.py
@@ -1,35 +1,41 @@
-questions = (("1.How many planets in the solar system?"),
-             ("2.Which is the hottest planet?"),
-             ("3.Who lays the biggest egg in the animal kingdom?"),
-             ("4.How many bones in the human body?"),
-             ("5.What is the most abundant gas the earth's atmosphere?"),
-             ("6.Electric bulb filament is made of ."),
-             ("7.What did James Watt invent?"),
-             ("8. When is the World Population Day?"),
-             ("9.The blueness of the sky is mainly due to:"))
+questions = [{"question": "How many planets in the solar system?",
+              "option": ["A. 11", "B. 10", "C. 8", "D. 7"],
+              "answer": "C"},
+             {"question": "Which is the hottest planet?",
+              "option": ["A. Mercury", "B. Venus", "C. Earth", "D. Mars"],
+              "answer": "B"},
+             {"question": ".Who lays the biggest egg in the animal kingdom?",
+              "option": ["A. Elephant", "B. Whale", "C. Ostrich", "D. Lion"],
+              "answer": "C"},
+             {"question": "How many bones in the human body?",
+              "option": ["A. 201", "B. 206", "C. 207", "D. 221"],
+              "answer": "B"},
+             {"question": ".What is the most abundant gas the earth's atmosphere?",
+              "option": ["A. Oxygen", "B. Hydrogen", "C. Nitrogen", "D. Helium"],
+              "answer": "C"},
+             {"question": "Electric bulb filament is made of .",
+              "option": ["A. Copper", "B. Aluminum", "C. Lead", "D. Tungsten"],
+              "answer": "D"},
+             {"question": "What did James Watt invent?",
+              "option": ["A. steam engine", "B. Steam boat",
+                         "C. Hot air balloon", "D. Diving bell"],
+              "answer": "A"},
+             {"question": "When is the World Population Day?",
+              "option": ["A. 31 May", "B. 4 October", "C. 10 December", "D. 11 July"],
+              "answer": "D"},
+             {"question": "The blueness of the sky is mainly due to:",
+              "option": ["A. The scattering of sunlight by air molecules", "B. The presence of water vapor", "C. Absorption of blue light by the air", "D. Emission of blue light by the atmosphere"],
+              "answer": "A"}]
 
-options = (("A. 11", "B. 10", "C. 8", "D. 7"),
-           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"),
-           ("A. Elephant", "B. Whale", "C. Ostrich", "D. Lion"),
-           ("A. 201", "B. 206", "C. 207", "D. 221"),
-           ("A. Oxygen", "B. Hydrogen", "C. Nitrogen", "D. Helium"),
-           ("A. Copper", "B. Aluminum", "C. Lead", "D. Tungsten"),
-           ("A. steam engine", "B. Steam boat",
-            "C. Hot air balloon", "D. Diving bell"),
-           ("A. 31 May", "B. 4 October", "C. 10 December", "D. 11 July"),
-           ("A. The scattering of sunlight by air molecules", "B. The presence of water vapor", "C. Absorption of blue light by the air", "D. Emission of blue light by the atmosphere"))
-
-answers = ("C", "B", "C", "B", "C", "D", "A", "D", "A")
 
 guesses = []
 score = 0
-question_num = 0
 
 for question in questions:
     print("-----------------------------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
+    print(question["question"])
+    for opt in question["option"]:
+        print(opt)
 
     while True:
         guess = input("enter you guess (A, B, C or D):").upper()
@@ -39,21 +45,20 @@ for question in questions:
         print("please enter value A,B,C or D only")
 
     guesses.append(guess)
-    if guess == answers[question_num]:
+    if guess == question["answer"]:
         score += 1
         print("CORRECT ANSWER")
     else:
         print("INCORRECT ANSWER")
-        print(f"The {answers[question_num]} is the correct answer")
-    question_num += 1
+        print(f"The {question["answer"]} is the correct answer")
 
 print("------------------------------")
-print("------------RESULT------------")
+print("-----------RESULT-------------")
 print("------------------------------")
 
-print("answers: ", end=" ")
-for answer in answers:
-    print(answer, end=" ")
+print("answer: ", end=" ")
+for ans in questions:
+    print(ans["answer"], end=" ")
 print()
 
 print("guesses: ", end=" ")
